3ac52751d0dc279ce290253f39714dbed4f4012c
MariaEduarda <mariaeduardadacruzferreira2007@gmail.com>
2026-05-19 15:03:02 -0300

    commit duda (estácom erro no HomeFragment.kt)

--- app/src/main/java/com/laraduda/task2/ui/HomeFragment.kt
+++ app/src/main/java/com/laraduda/task2/ui/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
 import com.laraduda.task2.R
 import com.laraduda.task2.databinding.FragmentHomeBinding
 import com.laraduda.task2.ui.adapter.ViewPagerAdapter
@@ -16,6 +18,8 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +31,26 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        initListeners()
 
         initTabs()
+    }
+
+    private fun initListeners(){
+        binding.btnLogout.setOnClickListener {
+            showBottomSheet(
+                titleButton = R.string.text_button_dialog_confirm_logout,
+                titleDialog = R.string.text_title_dialog_confira_logout,
+                message=getString(R.string.text_message_dialog_confirm_logout),
+                onClick ={
+                    auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_autentication)
+                }
+            )
+        }
     }
 
     private fun initTabs(){
